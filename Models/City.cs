9eb78d426122e08a8cc66e7c0cb36cb9dca9c234
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-06-16 11:32:12 -0500

    Added /cities handlers

--- Models/City.cs
+++ Models/City.cs
@@ -7,4 +7,6 @@ public class City
     public int Id { get; set; } 
     [Required]
     public string Name { get; set; } 
+    public List<Log> Logs { get; set; } 
+    public List<Recommendation> Recommendations { get; set; }
 }
--- Models/DTOs/RecommendationDTO.cs
+++ Models/DTOs/RecommendationDTO.cs
@@ -10,4 +10,5 @@ public class RecommendationDTO
     [Required]
     public string Name { get; set; }
     public string Description { get; set; }
+    public UserDTO User { get; set; }
 }
