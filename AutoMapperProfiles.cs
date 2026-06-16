5cc4710066f56650e9a152460f02c4396e5bd9a9
Jude Andersen <jude.ottilie@gmail.com>
2026-06-16 10:19:16 -0500

    mapper returns

--- AutoMapperProfiles.cs
+++ AutoMapperProfiles.cs
@@ -7,9 +7,14 @@ public class AutoMapperProfiles : Profile
     public AutoMapperProfiles()
     {
         CreateMap<User, UserDTO>();
+        CreateMap<UserDTO, User>();
         CreateMap<City, CityDTO>();
+        CreateMap<CityDTO, City>();
         CreateMap<Log, LogDTO>();
+        CreateMap<LogDTO, Log>();
         CreateMap<Recommendation, RecommendationDTO>();
+        CreateMap<RecommendationDTO, Recommendation>();
         CreateMap<UpVote, UpVoteDTO>();
+        CreateMap<UpVoteDTO, UpVote>();
     }
 }
